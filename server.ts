6514bae3b7c48bc71a6b3d8e73430c0239212e28
Niranjay-FH <niranjay.a@foodhub.com>
2026-01-14 18:48:00 +0530

    fix: calculate tier automatically
    ensure tier calculation is performed automatically.

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/api/loyalty', loyaltyRoutes);
 
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
